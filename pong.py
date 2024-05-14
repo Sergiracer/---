d7781ebadddf66c5559176016d1792c6dcab0e3b
Sergiracer <168576498+Sergiracer@users.noreply.github.com>
2024-05-14 19:53:09 +0300

    Update pong.py

--- pong.py
+++ pong.py
@@ -2,13 +2,16 @@ from pygame import *
 from random import *
 
 font.init()
-font1 = font.Font(None, 120)
+lose1 = font.Font(None, 60)
+lose2 = font.Font(None, 60)
+restart = font.Font(None, 30)
 mixer.init()  
 window = display.set_mode((700, 500))
 clock = time.Clock()
 
-lose = font1.render('YOU LOSE!', True, (225, 0, 0))
-
+lose1 = lose1.render('PLAYER 1 LOSE!', True, (225, 0, 0))
+lose2 = lose2.render('PLAYER 2 LOSE!', True, (225, 0, 0))
+restart = restart.render('Press SPACE to retart', True, (225, 0, 0))
 FPS = 60
 
 display.set_caption('Game')
@@ -41,18 +44,42 @@ class Player(GameSprite):
 
 player1 = Player("bar1.png", 10, 670, 250, 20, 100)
 player2 = Player("bar2.png", 10, 10, 250, 20, 100)
-ball = Player("ball.png", 10, 300, 250, 40, 40)
-game = True
+ball = GameSprite("ball.png", 10, 300, 250, 40, 40)
 
+sp_x = 3
+sp_y = 3
+game = True
+finish = False
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-    window.blit(bg, (0, 0))
-    player1.update1()
-    player1.reset()
-    ball.reset()
-    player2.update2()
-    player2.reset()
+        if e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                finish = False
+                sp_x *= -1
+                ball.rect.x = 200
+    if not finish:
+        window.blit(bg, (0, 0))
+        player1.update1()
+        player1.reset()
+        ball.reset()
+        player2.update2()
+        player2.reset()
+        ball.rect.x += sp_x
+        ball.rect.y += sp_y
+        if ball.rect.y <= 0 or ball.rect.y >= 460:
+            sp_y *= -1
+        if sprite.collide_rect(ball, player1) == True or sprite.collide_rect(ball, player2) == True:
+            sp_x *= -1
+        if ball.rect.x < 0 :
+            finish = True
+            window.blit(lose1, (185, 220))
+            window.blit(restart, (220, 465))
+        if ball.rect.x > 660 :
+            finish = True
+            window.blit(lose2, (185, 220))
+            window.blit(restart, (220, 465))
+    
     clock.tick(FPS)
     display.update()
